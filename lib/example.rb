80e47959706750d476dba27abdf6a7887ccceaab
Matias Damonte <mdamonte88@gmail.com>
2020-06-07 01:13:28 +0000

    Automatically backed up by Learn

---
+++ lib/example.rb
@@ -0,0 +1,5 @@
+num_of_attendees = 547
+puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
+
+answer = "< fill in your answer here >"
+puts "A group of flamingos is called a #{answer}."
